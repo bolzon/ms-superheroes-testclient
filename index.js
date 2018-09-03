b36277d842e532f05423dcf7046ab30f41cd928c
Bolzon <blzn@mail.ru>
2018-09-03 11:26:15 -0300

    Initial commit pushing socket client

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const io = require('socket.io-client');
+const socket = io('http://localhost:4440');
+
+socket.on('connect', () => {
+	console.log('Client connected');
+});
+
+socket.on('auditEvent', message => {
+	console.log('New audit event received:');
+	console.log(JSON.stringify(message, null, 3));
+});
